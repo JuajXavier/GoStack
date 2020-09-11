616ff8d0c3b43fc5bb72b07d6aa00e2f0c138030
João Xavier <jvitorxavier@gmail.com>
2020-09-11 17:41:38 -0300

    Add github api connection with axios.

--- Basics/github-explorer/src/pages/Dashboard/index.tsx
+++ Basics/github-explorer/src/pages/Dashboard/index.tsx
@@ -1,63 +1,66 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImage from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
  
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    // add new repo through github api -> save State
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    // console.log(response.data)
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImage} alt='Github Explorer' />
       <Title>Explore repositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório"/>
+      <Form onSubmit={handleAddRepository}>
+        <input
+         value={newRepo}
+         onChange={(event) => setNewRepo(event.target.value)}
+         placeholder="Digite o nome do repositório"
+         />
         <button>Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img 
-            src="https://avatars2.githubusercontent.com/u/60194147?s=460&u=7b15cc0fdb6fa5fe381a9aceddbe6cde2f748f89&v=4" 
-            alt="Juaj"
-          />
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img 
+              src={repository.owner.avatar_url} 
+              alt={repository.owner.login}
+            />
 
-          <div>
-            <strong>JuajXavier/Ecoleta</strong>
-            <p>App de coleta.</p>
-          </div>
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20}/>
-        </a>
-
-        <a href="teste">
-          <img 
-            src="https://avatars2.githubusercontent.com/u/60194147?s=460&u=7b15cc0fdb6fa5fe381a9aceddbe6cde2f748f89&v=4" 
-            alt="Juaj"
-          />
-
-          <div>
-            <strong>JuajXavier/Ecoleta</strong>
-            <p>App de coleta.</p>
-          </div>
-
-          <FiChevronRight size={20}/>
-        </a>
-        
-        <a href="teste">
-          <img 
-            src="https://avatars2.githubusercontent.com/u/60194147?s=460&u=7b15cc0fdb6fa5fe381a9aceddbe6cde2f748f89&v=4" 
-            alt="Juaj"
-          />
-
-          <div>
-            <strong>JuajXavier/Ecoleta</strong>
-            <p>App de coleta.</p>
-          </div>
-
-          <FiChevronRight size={20}/>
-        </a>
+            <FiChevronRight size={20}/>
+          </a>
+        ))}
 
       </Repositories>
     </> 
